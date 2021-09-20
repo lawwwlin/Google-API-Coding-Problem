8195e4c9fcb42634f6c7822ba9c5c78e5429bbe1
Lawrence Lin <80787904+lawwwlin@users.noreply.github.com>
2021-09-20 14:06:12 -0700

    fix bug: writing data to wrong cell due to string additions

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const SHEET_ID = '1N0IHTEEB7jTqE8YaJeW1BKXC9FrRLJLDDIdWkZWvhb8';
 // Google Drive id of the main drive containing sub-folders of dog photos
 const DRIVE_ID = '10_HRQGt3nF2S3fc9-JxW4zvxMqIwk0XH';
 
-const { getFilesInFolder, getSheetData, createFolder, copyFilesTo, matchFileWithSheet } = require('./helpers');
+const { getFilesInFolder, getSheetData, createFolder, copyFilesTo, matchFileWithSheet, writeToSheet } = require('./helpers');
 
 
 // call Google API
@@ -22,29 +22,33 @@ const run = async () => {
   const photosArray = [].concat.apply([], photosNestedArray);
   
   // create new folder in main drive to store copies of files
-  // const newFolderId = await createFolder(DRIVE_ID);
+  const newFolderId = await createFolder(DRIVE_ID);
   
   // copy renamed files to new folder
-  // const copiedNestedPhotosArray = photosArray.forEach(photo => {
-  //   copyFilesTo(photo, '19f8Hwbsc9_BBEwOVyNM_47083OYsHgNQ');
-  // });
+  const copiedNestedPhotosArray = await Promise.all(photosArray.map(async (file) => {
+    const photo = await copyFilesTo(file, newFolderId);
+    return photo;
+  }));
 
   // array of files with id, name, mimetype
-  // const copiedPhotosArray = [].concat.apply([], copiedNestedPhotosArray);
+  const copiedPhotosArray = [].concat.apply([], copiedNestedPhotosArray);
   // console.log(copiedPhotosArray);
   
   // get breed name from sheets
   const sheetDataArray = await getSheetData(SHEET_ID);
-  console.log("sheetDataArray", sheetDataArray);
+  // console.log("sheetDataArray", sheetDataArray);
 
   // match each dog name entry in sheet with copied image file
   // then write file id into sheet
   for (const i in sheetDataArray) {
     const sheetDogName = sheetDataArray[i][0];
-    const found = copiedPhotosArray.find(photo => matchFileWithSheet(photo.name, sheetDogName));
-    
+    console.log('dog name in sheets', sheetDogName, 'cell', parseInt(i) + 2);
+    const fileFound = copiedPhotosArray.find((file) => matchFileWithSheet(file.name, sheetDogName));
+    console.log('filefound?', fileFound);
+    if (fileFound) {
+      writeToSheet(SHEET_ID, `B${parseInt(i) + 2}`, fileFound.id);
+    }
   }
-
 };
 
 run();
